66fe2295066adff3c2e0f837c4bf22606a21d8ac
shaunbuchan <1812315@rgu.ac.uk>
2019-02-15 15:08:59 +0000

    init

---
+++ accounts.php
@@ -0,0 +1,14 @@
+<?php
+$accounts=array(	"admin"=>"hello",
+    "john"=>"world",
+    "mary"=>"good",
+    "peter"=>"morning"
+);
+?>
+
+/**
+ * Created by PhpStorm.
+ * User: 1812315
+ * Date: 15/02/2019
+ * Time: 15:08
+ */
